f112ff1664084f4f23da03f4078b80dc709045dd
Alan Moreno <alan.dario.moreno@hotmail.com>
2024-06-21 16:52:56 -0300

    Se agregó la lista de ejercicios con algunos datos de la bd

--- App.js
+++ App.js
@@ -1,37 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LoginPage from './screens/Login/LoginPage';
-import ExcerciseListPage from './screens/ExcerciseList/ExcerciseListPage';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import LoginPage from "./screens/Login/LoginPage";
+import ExcerciseListPage from "./screens/ExcerciseList/ExcerciseListPage";
 
 const Stack = createNativeStackNavigator();
-
-
-
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen 
-          name='Bottom Navigation'
+        <Stack.Screen
+          name="Bottom Navigation"
           component={BottomTabNavigator}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Login'
+          name="Login"
           component={LoginPage}
-          options={{headerShown: false}}
-          />
-<Stack.Screen 
-        name='ExcerciseListPage'
-        component={ExcerciseListPage}              
-
-/>
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ExcerciseListPage"
+          component={ExcerciseListPage}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
--- services/TypeOfExcercises/index.js
+++ services/TypeOfExcercises/index.js
@@ -1,16 +1,52 @@
 
+//Segun mati. Crear en la base los tipos, traerlos todos y mapearlos para devolver solo los correctos.
+// despues entregar el array con los nombres filtrados y renderizarlos
+
+//uniq = [...new Set(array)]; PARA ELIMINAR LOS NOMBRES DUPLICADOS EN LA BASE
 
 //PROMESIFICAR agregar conexión con la Base final
-const listaDeEjercicios = [
-    { name: "Flexiones", type: "Entrenamiento de fuerza" },
-    { name: "Sentadillas", type: "Entrenamiento de fuerza" },
-    { name: "Correr", type: "Cardio" },
-    { name: "Planchas", type: "Entrenamiento del core" },
-    { name: "Dominadas", type: "Entrenamiento de fuerza" },
-    { name: "Ciclismo", type: "Cardio" },
-    { name: "Burpees", type: "Entrenamiento de cuerpo completo" },
-    { name: "Saltos de tijera", type: "Cardio" },
-    { name: "Abdominales", type: "Entrenamiento del core" },
-    { name: "Zancadas", type: "Entrenamiento de fuerza" }
-  ];
-export default listaDeEjercicios
+const listOfExcercises = [
+  {  type: "Entrenamiento de fuerza" },
+  {  type: "Cardio" },
+  {  type: "Entrenamiento del core" },
+  {  type: "Entrenamiento de cuerpo completo" },
+];
+
+// OBTENGO LA LISTA DE EJERCICIOS COMPLETA Y LA MAPEO EN UN OBJETO DE TIPOS DE EJERCICIOS
+const typeOfExcercises = listOfExcercises.reduce((acc, excercise) => {
+  const { type } = excercise;
+
+  if (!acc[type]) {
+    acc[type] = [];
+  }
+  acc[type].push(excercise);
+  return acc;
+}, {});
+
+// DESESTRUCTURO EL OBJETO Y LO CONVIERTO EN ARRAY PARA MANEJARLO
+
+const cant = Object.keys(typeOfExcercises).length;
+// console.log(cant);
+
+const group = () => {
+  let array = [];
+
+  for (let index = 0; index < cant; index++) {
+    let group = Object.keys(typeOfExcercises)[index];
+
+    array.push(group);
+  }
+  return array;
+};
+// console.log("ESTOY")
+
+// const array = group()
+// console.log(typeof array[1]);
+
+const getGroupExcersices = () => {
+  return new Promise((resolve, reject) => {
+    return resolve(listOfExcercises);
+  });
+};
+
+export default { getGroupExcersices };
