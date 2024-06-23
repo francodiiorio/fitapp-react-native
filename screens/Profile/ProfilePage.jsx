3b4da174fdf5f09af5780ac6a2f960227a0f6100
Franco Di Iorio <francodiiorio@MacBook-Pro-de-Franco.local>
2024-06-23 13:41:51 -0300

    Deslogueo de usuario

--- screens/Profile/ProfilePage.jsx
+++ screens/Profile/ProfilePage.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useContext } from "react";
+import { Text, View, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LoginPage from "../Login/LoginPage";
+import { getAuth, signOut } from "firebase/auth";
+import { appFirebase } from "../../credentials";
+import AuthContext from "../../services/AuthContext";
+
+const auth = getAuth(appFirebase)
 
 const ProfilePage = () => {
+
+    const { authData, setAuthData } = useContext(AuthContext)
+
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        setAuthData(null)
+        console.log("User signed out!");
+      })
+      .catch((error) => {
+        console.error("Sign out error:", error);
+      });
+  };
+
     return (
         <SafeAreaView>
-            <LoginPage/>
+            {authData ? (
+                <View>
+                    <Text>Hola! {authData.user?.email}</Text>
+                    <Button title="Logout" onPress={handleLogout} />
+                </View>
+            ) : (
+                <LoginPage/>
+            )}
         </SafeAreaView>
     )
 }
